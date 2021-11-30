daa8e758ab1f8f163c47fd44394c496559af4cb7
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-30 21:23:00 +0200

    Can now show albums by artist

--- console.py
+++ console.py
@@ -5,15 +5,22 @@ from models.artist import Artist
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
 
-# album_repository.delete_all()
-# artist_repository.delete_all()
+album_repository.delete_all()
+artist_repository.delete_all()
 
 artist_1 = Artist("Anderson .Paak")
 artist_repository.save(artist_1)
+artist_2 = Artist("Test Artist")
+artist_repository.save(artist_2)
 album_1 = Album("Malibu", "R&B", artist_1)
 album_repository.save(album_1)
+album_2 = Album("Test 1", "Test genre 1", artist_2)
+album_repository.save(album_2)
+album_3 = Album("Test 2", "Test genre 2", artist_2 )
+album_repository.save(album_3)
 
-artist_repository.select(1)
-album_repository.select(1)
+# artist_repository.select(1)
+# album_repository.select(1)
+print (artist_repository.albums(artist_2))
 
 pdb.set_trace()
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -43,3 +43,16 @@ def select_all():
 def delete_all():
     sql = "DELETE FROM artists"
     run_sql(sql)
+
+#SELECT ALL ALBUMS
+
+def albums(artist):
+    albums = []
+    sql = "SELECT * FROM albums WHERE artist_id = %s"
+    values = [artist.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        album = Album(row['title'], row['genre'], artist, row['id'])
+        albums.append(album)
+    return albums
