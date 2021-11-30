fe6f8d7432b1e18a0d98f91a1c93ab4b9f578379
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-30 20:38:56 +0200

    Can now save album

--- console.py
+++ console.py
@@ -3,6 +3,9 @@ from models.album import Album
 from models.artist import Artist
 
 import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
 
 artist_1 = Artist("Anderson .Paak")
 artist_repository.save(artist_1)
+album_1 = Album("Malibu", "R&B", artist_1)
+album_repository.save(album_1)
---
+++ repositories/album_repository.py
@@ -0,0 +1,13 @@
+from db.run_sql import run_sql
+
+from models.album import Album
+from models.artist import Artist
+
+#Create
+def save(album):
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
